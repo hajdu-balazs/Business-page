2d27a53525bdd7cd8f354ce182ca51b837750105
bhajdu <bhajdu85@gmail.com>
2021-02-19 11:39:33 +0000

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+ function hideMenu() {
+   let menuOpen = document.querySelector("#toggle").checked;
+
+   if ((menuOpen = true)) {
+     document.querySelector("#toggle").checked = false;
+   }
+ }
+ window.addEventListener("scroll", hideMenu);
+
+
+
+
 // smooth scroll
 $(document).ready(function () {
   $("menu a").on("click", function (event) {
